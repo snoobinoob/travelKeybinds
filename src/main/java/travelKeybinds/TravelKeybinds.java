b1bc8c564e2bc0274f2941331611c57a0fc3db3b
Snoobinoob <cheeseycheese99@gmail.com>
2023-02-21 19:17:05 -0500

    Initial commit

---
+++ src/main/java/travelKeybinds/TravelKeybinds.java
@@ -0,0 +1,11 @@
+package travelKeybinds;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class TravelKeybinds {
+
+    public void init() {
+        System.out.println("Hello world from Travel Keybinds!");
+    }
+}
